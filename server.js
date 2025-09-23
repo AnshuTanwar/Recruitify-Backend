3e15fc07ee2376683a9456aa000bf234b01d17fc
Aanshu Tanwar <aanshutanwar07@gmail.com>
2025-09-23 23:46:12 +0530

    Added protect, role and a centralised middleware

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const passport = require("passport");
 require("./config/passport");
 
 const authRoutes = require("./routes/authRoutes");
+const errorHandler = require("./middlewares/errorHandler");
 
 const app = express();
 app.use(express.json());
@@ -39,6 +40,8 @@ async function connectDB() {
 
 // Routes
 app.use("/api/auth", authRoutes);
+
+app.use(errorHandler);
 
 module.exports = app;
 

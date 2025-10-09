5b52b4fa4fb0c4186bd7dd5e50e5770e3bbcb05f
Aanshu Tanwar <aanshutanwar07@gmail.com>
2025-10-10 00:59:52 +0530

    deployment commits

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const errorHandler = require("./middlewares/errorHandler");
 const app = express();
 
 const corsOptions = {
-    origin: ['http://localhost:5173', 'https://recruitify-pi.vercel.app/'],
+    origin: ['http://localhost:5173', 'https://recruitify-pi.vercel.app'],
     credentials: true,
     optionsSuccessStatus: 200,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],

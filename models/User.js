697fb317ad005d3b85c8ea4d150254cadbd6d31c
Aanshu Tanwar <aanshutanwar07@gmail.com>
2025-10-10 00:24:05 +0530

    Added User.js model

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    fullName : {
+        type: String,
+        required: true,
+    },
+    email : {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true
+    },
+    password : {
+        type: String,
+        required: true
+    },
+    role : {
+        type: String,
+        enum: ["Admin", "Candidate", "Recruiter"],
+        required: true
+    },
+    provider : {
+        type: String,
+        enum: ["local", "google"],
+        default: "local"
+    },
+    avatar : {
+        type: String
+    },
+    isActive : {
+        type: Boolean,
+        default: true
+    },
+    lastLogin: {
+        type: Date
+    },
+    refreshTokens: { type: [String], default: [] }
+}, {
+    timestamps: true,
+    discriminatorKey: "role"
+});
+
+module.exports = mongoose.model('User', userSchema);

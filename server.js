d9d76cf6df692db0d24cb399e06702f8abf49320
Aanshu Tanwar <aanshutanwar07@gmail.com>
2025-10-09 23:02:10 +0530

    deom

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const errorHandler = require("./middlewares/errorHandler");
 const app = express();
 
 const corsOptions = {
-    origin: ['http://localhost:5173', '*'],
+    origin: ['http://localhost:5173', '*', 'https://main.d2vm8xyjprv7ng.amplifyapp.com/'],
     credentials: true,
     optionsSuccessStatus: 200,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],

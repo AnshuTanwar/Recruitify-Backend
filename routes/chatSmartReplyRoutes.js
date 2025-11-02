715eebb814ae01611b55295f75d34a0b8019ed7f
Aanshu Tanwar <aanshutanwar07@gmail.com>
2025-11-02 23:25:03 +0530

    chat route added

--- routes/chatSmartReplyRoutes.js
+++ routes/chatSmartReplyRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { protect } = require("../middlewares/authMiddleware");
-const { authorizeRoles } = require("../middlewares/roleMiddleware");
+const authorizeRoles = require("../middlewares/roleMiddleware");
 const chatSmartReplyController = require("../controllers/chatSmartReplyController");
 
 // Candidate-only route

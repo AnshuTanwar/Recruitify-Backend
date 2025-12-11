5c2339228daf99cedbfd2d0b915bdee237e38b40
Aanshu Tanwar <aanshutanwar07@gmail.com>
2025-12-11 23:03:26 +0530

    Increase JSON body size limit to 2mb

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ connectRedis()
 
 // Middlewares
 app.use(cors(corsOptions));
-app.use(express.json());
+app.use(express.json({ limit: '2mb' }));
 app.use(cookieParser());
 app.use(passport.initialize());
 
